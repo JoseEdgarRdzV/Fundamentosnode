9be387e77f534104d0a6e3f4151743390b3b7cd0
SinNombre 89 <je.rodriguezvazquez@ugto.mx>
2024-01-25 09:27:45 -0600

    instalamos firabase

--- servidor.js
+++ servidor.js
@@ -1,6 +1,24 @@
 const express = require('express')
+import { initializeApp } from "firebase/app";
 const app = express()
 const port = 6000
+
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyByZ1aNcE0jEgpamL4DrcbfKcFSuqaS63k",
+  authDomain: "crud-edgar-proyecto1.firebaseapp.com",
+  projectId: "crud-edgar-proyecto1",
+  storageBucket: "crud-edgar-proyecto1.appspot.com",
+  messagingSenderId: "805820307299",
+  appId: "1:805820307299:web:f073c46eec6d7dcd116556"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 app.get('/', (req,res) => {
     res.send('Respuesta de Raiz')
